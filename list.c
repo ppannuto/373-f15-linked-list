d27f162a8534bee9e30ab127ef8a9b3524737777
Pat Pannuto <pat.pannuto@gmail.com>
2015-09-07 16:30:22 -0400

    Add a list implementation

--- list.c
+++ list.c
@@ -11,7 +11,35 @@ list_t* insert_sorted(list_t* head, list_t* new_element) {
 	assert(head != NULL);
 	assert(new_element != NULL);
 
+	if (new_element->index <= head->index) {
+		new_element->next = head;
+		return new_element;
+	}
+
+	list_t* current = head;
+	while (current->next != NULL) {
+		if (new_element->index <= current->next->index) {
+			new_element->next = current->next;
+			current->next = new_element;
+			return head;
+		}
+		current = current->next;
+	}
+
+	current->next = new_element;
+	new_element->next = NULL;
 	return head;
+}
+
+list_t* reverse_internal(list_t* current, list_t* current_next) {
+	if (current_next == NULL) {
+		return current;
+	}
+
+	list_t* new_head;
+	new_head = reverse_internal(current_next, current_next->next);
+	current_next->next = current;
+	return new_head;
 }
 
 // Reverses the order of the list starting at `head` and returns a pointer to
@@ -19,6 +47,10 @@ list_t* insert_sorted(list_t* head, list_t* new_element) {
 list_t* reverse(list_t* head) {
 	assert(head != NULL);
 
-	return head;
+	// (Ab)use the stack to not deal with dynamic memory
+	list_t* new_head;
+	new_head = reverse_internal(head, head->next);
+	head->next = NULL;
+	return new_head;
 }
 
